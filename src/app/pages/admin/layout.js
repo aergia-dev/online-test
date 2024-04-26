7b55e4c899882335ee6a6fe012a54e964c7c92c9
aergia <aregia.dev@gmail.com>
2024-04-26 17:24:59 +0900

    change admin layout

--- src/app/pages/admin/layout.js
+++ src/app/pages/admin/layout.js
@@ -1,17 +1,11 @@
 import Navbar from './navbar'
-import Sidebar from './sidebar'
 
 export default function AdminLayout({ children }) {
   return (
     <section>
       <div className="flex flex-col">
         <Navbar />
-        <div >
-          <div className="flex flex-row">
-            <Sidebar />
-            {children}
-          </div>
-        </div>
+        {children}
       </div>
     </section >
   )
--- src/app/pages/admin/navbar.js
+++ src/app/pages/admin/navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
     <nav className="flex items-center justify-between flex-wrap bg-gray-700 p-6">
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div className="text-sm lg:flex-grow justify-between">
-          <a href="/pages/admin/questions" className="block mt-4 lg:inline-block lg:mt-0 text-white font-bold hover:text-white mr-4">
+          <a href="/pages/admin/manageData" className="block mt-4 lg:inline-block lg:mt-0 text-white font-bold hover:text-white mr-4">
             문제 관리 
           </a>
           <a href="/pages/admin/monitoring" className="block mt-4 lg:inline-block lg:mt-0 text-white font-bold hover:text-white mr-4">
