17df3e0aecfe6d81507645bbe576ff379c842f83
aergia <aergia.dev@gmail.com>
2024-05-27 17:02:01 +0900

    fix - dup key of element

--- src/app/adminPage/monitoring/testResult/page.js
+++ src/app/adminPage/monitoring/testResult/page.js
@@ -32,26 +32,26 @@ export default function TestResult() {
     const [qPreview, setQPreview] = useState('');
 
 
-const loadMathJax = () => {
-    return new Promise((resolve, reject) => {
-      if (window.MathJax) {
-        resolve(window.MathJax);
-      } else {
-        const script = document.createElement('script');
-        script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
-        script.async = true;
-        script.onload = () => {
-          if (window.MathJax) {
-            resolve(window.MathJax);
-          } else {
-            reject(new Error('MathJax failed to initialize'));
-          }
-        };
-        script.onerror = () => reject(new Error('MathJax failed to load'));
-        document.head.appendChild(script);
-      }
-    });
-  };
+    const loadMathJax = () => {
+        return new Promise((resolve, reject) => {
+            if (window.MathJax) {
+                resolve(window.MathJax);
+            } else {
+                const script = document.createElement('script');
+                script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+                script.async = true;
+                script.onload = () => {
+                    if (window.MathJax) {
+                        resolve(window.MathJax);
+                    } else {
+                        reject(new Error('MathJax failed to initialize'));
+                    }
+                };
+                script.onerror = () => reject(new Error('MathJax failed to load'));
+                document.head.appendChild(script);
+            }
+        });
+    };
 
 
     useEffect(() => {
@@ -234,19 +234,20 @@ const loadMathJax = () => {
             <div id="managingTestResult"
                 className="flex flex-col">
                 <p className='font-bold'>시험 결과 리스트</p>
-                {titles && titles.map(title => {
+                {titles && titles.map((title, idx) => {
                     return (
                         <div className='flex flex-row space-x-4'
-                            key={title + 'div'}>
-                            <button key={title + 'delBtn'}
+                            key={title + '_' + idx + 'div'}>
+                            <button key={title + '_' + 'delBtn'}
                                 onClick={() => { handleRemoveTitle(title) }}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                 </svg>
                             </button>
-                            <p key={title}
-                                onClick={() => { handleTitleClick(title) }}>{title}
-                            </p>
+                            <button key={title + '_' + idx}
+                                onClick={() => { handleTitleClick(title) }}>
+                                {title}
+                            </button>
                         </div>
                     )
                 })}
@@ -270,7 +271,7 @@ const loadMathJax = () => {
                         <tbody className='border border-gray-600'>
                             {testResult.userResult.length > 0 && testResult.userResult.map(({ userInfo, questionCnt, resultQuestion, surveyResult }, idx) => (
                                 <tr className='text-center'
-                                    key={idx + '_' + userInfo.userName + '_' + userInfo.userId + '_' + userInfo.userAffiliation}>
+                                    key={idx + '__' + userInfo.userName + '_' + userInfo.userId + '_' + userInfo.userAffiliation}>
 
                                     <td className='border border-gray-600'
                                         key={idx + '_del_' + userInfo.userName}>
@@ -282,15 +283,15 @@ const loadMathJax = () => {
                                         </button>
                                     </td>
                                     <td className='border border-gray-600'
-                                        key={idx + '_' + userInfo.userName}>
+                                        key={idx + '_userName_' + userInfo.userName}>
                                         {userInfo.userName}
                                     </td>
                                     <td className='border border-gray-600'
-                                        key={idx + '_' + userInfo.userId}>
+                                        key={idx + '_userId_' + userInfo.userId}>
                                         {userInfo.userId}
                                     </td>
                                     <td className='border border-gray-600'
-                                        key={idx + '_' + userInfo.userAffiliation}>
+                                        key={idx + '_userAffiliation' + userInfo.userAffiliation}>
                                         {userInfo.userAffiliation}
                                     </td>
                                     <td className='border border-gray-600'
@@ -299,11 +300,11 @@ const loadMathJax = () => {
                                     </td>
                                     <td className='border border-gray-600'
                                         key={idx + '_' + "endTime"}>
-                                        {userInfo.endTime === undefined ? '미제출 ' : '제출'}
+                                        {userInfo.testEndTime === undefined ? '미제출 ' : '제출'}
                                     </td>
                                     <td className='border border-gray-600'
                                         key={idx + '_' + "survey"}>
-                                        {surveyResult === undefined ? '미제출 ' : '제출'}
+                                        {userInfo.surveyEndTime === undefined ? '미제출 ' : '제출'}
                                     </td>
                                     <td className='border border-gray-600 px-4'>
                                         <button >
