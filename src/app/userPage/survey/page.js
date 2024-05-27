0a43d415f16bef52f107300e1b799149126ca649
aergia <aergia.dev@gmail.com>
2024-05-28 00:12:18 +0900

    add survey validation

--- src/app/userPage/survey/page.js
+++ src/app/userPage/survey/page.js
@@ -10,57 +10,60 @@ export default function Survey() {
     const [fin, setFin] = useState(false);
 
     const onActionItem1 = ({ key, rowIdx, colIdx, choiceIdx, uuid }) => {
-        console.log("onAction", key, rowIdx, colIdx, choiceIdx, uuid);
         const answerIdx = choiceIdx + 1;
         const newSurveyForm = { ...surveyForm };
-
-        console.log('survey', key)
-        console.log('survey', newSurveyForm[key])
-
         const foundIdx = newSurveyForm[key].findIndex((item) => item['uuid'] === uuid);
         const existIdx = newSurveyForm[key][foundIdx].answer.findIndex(answer => answer === answerIdx);
 
-        console.log('existIdx', existIdx)
         if (existIdx !== -1) {
-            console.log("$$", newSurveyForm[key][foundIdx].answer)
             newSurveyForm[key][foundIdx].answer.splice(existIdx, 1);
-            console.log("$$%%", newSurveyForm[key][foundIdx].answer)
         }
         else {
             newSurveyForm[key][foundIdx].answer.push(choiceIdx + 1);
         }
-
         setSurveyForm(newSurveyForm);
     }
 
     const onActionItem2 = ({ key, rowIdx, colIdx, choiceIdx, uuid }) => {
-        // console.log("onActionItem2", key, rowIdx, colIdx, choiceIdx, uuid);
         const newSurveyForm = { ...surveyForm };
         const foundIdx = newSurveyForm[key][rowIdx]['secondRow'].findIndex((item) => item.uuid === uuid);
-        // console.log('found', newSurveyForm[key][rowIdx]['secondRow'][foundIdx]);
         newSurveyForm[key][rowIdx]['secondRow'][foundIdx].choice = choiceIdx + 1;
         setSurveyForm(newSurveyForm);
-        // console.log('found', newSurveyForm[key][rowIdx]['secondRow'][foundIdx]);
     }
 
     const onActionItem3 = ({ key, rowIdx, colIdx, choiceIdx, uuid }) => {
-        // console.log("onActionItem3", key, rowIdx, colIdx, choiceIdx, uuid);
         const newSurveyForm = { ...surveyForm };
         const foundIdx = newSurveyForm[key].findIndex((item) => item['uuid'] === uuid);
-        // console.log("found idx", foundIdx);
         newSurveyForm[key][foundIdx].choice[colIdx] = choiceIdx + 1;
         setSurveyForm(newSurveyForm);
     }
 
     const onActionItem4 = ({ key, text }) => {
         const newSurveyForm = { ...surveyForm };
-        // console.log("onActionItem4", key, text);
         newSurveyForm[key]['text'] = text;
         setSurveyForm(newSurveyForm);
     }
 
     const validation = (surveyForm) => {
-        return true;
+        console.log('surveyForm', surveyForm)
+        const item1NotAnswered = surveyForm.item1.filter(item => item.answer.length === 0);
+        const item2NotAnswered = surveyForm.item2Row.filter(row => {
+            row.secondRow.filter(row => row.choice === 0)
+        });
+        //info: remove first column that is just title.
+        const item3AnswerCnt = surveyForm.item3Col.length - 1;
+        console.log('item3AnswerCnt', item3AnswerCnt);
+        const item3NotAnswered = surveyForm.item3Row.filter(item => item.choice.length !== item3AnswerCnt)
+
+        console.log('item1NotAnswered', item1NotAnswered);
+        console.log('item2NotAnswered', item2NotAnswered);
+        console.log('item3NotAnswered', item3NotAnswered);
+
+        const ret = item1NotAnswered.length === 0 &&
+            item2NotAnswered.length === 0 &&
+            item3NotAnswered.length === 0; 
+
+        return ret;
     }
 
     const onSave = async () => {
