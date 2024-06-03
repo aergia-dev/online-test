e1366a55497fe3387cb93ac46b27dfd0778c2e0b
aergia <aergia.dev@gmail.com>
2024-06-03 10:57:48 +0900

    fix bug - show time

--- src/app/funcs/userInfo.js
+++ src/app/funcs/userInfo.js
@@ -13,17 +13,12 @@ export default function UserInfo({ session }) {
         const startTime = Date.now();
         const intervalId = setInterval(() => {
             const s = Math.floor((Date.now() - startTime) / 1000);
-            setElapsedTime({
-            });
-
             setElapsedTime(Math.floor((Date.now() - startTime) / 1000));
         }, 1000);
 
         return () => clearInterval(intervalId);
-    });
+    }, []);
 
-
-    const second = 0;
     if (session) {
         return (
             <nav className='fixed top-0 left-0 right-0 bg-gray-600 text-white shadow-md z-50 '>
